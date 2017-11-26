299a25672e6c1f5c3f0c4436b26c9ed260da45ab
Ravinder Kumar <ravinder.kumar.apj@gmail.com>
2017-11-26 09:48:30 +0530

    Add home page link to logo

--- template-parts/header.php
+++ template-parts/header.php
@@ -3,7 +3,7 @@
 	<div class="for-sticky">
 		<!--LOGO-->
 		<div class="col-md-2 col-xs-6 logo">
-			<a href="index.html"><img alt="logo" class="logo-nav" src="<?php echo get_template_directory_uri(); ?>/assets/images/logo.png"></a>
+			<a href="<?php echo esc_url( home_url('/') ); ?>"><img alt="logo" class="logo-nav" src="<?php echo get_template_directory_uri(); ?>/assets/images/logo.png"></a>
 		</div>
 		<!--/.LOGO END-->
 	</div>
