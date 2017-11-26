e5260988597d1ce0b1d7a171d9ee78c9094d55da
Ravinder Kumar <ravinder.kumar.apj@gmail.com>
2017-11-26 09:48:17 +0530

    refactor index.php

--- index.php
+++ index.php
@@ -1,18 +1,26 @@
 <?php get_header(); ?>
-		<?php get_template_part( 'template-parts/preloader' ); ?>
-		<?php get_template_part( 'template-parts/header' ); ?>
+<?php get_template_part( 'template-parts/preloader' ); ?>
+<?php get_template_part( 'template-parts/header' ); ?>
 
-		<!--CONTENT WRAP-->
-		<div class="content-wrap">
-			<!--CONTENT-->
-			<div class="content">
-				<?php get_template_part( 'template-parts/about' ); ?>
-				<?php get_template_part( 'template-parts/contact' ); ?>
-				<?php get_template_part( 'template-parts/social-media-footer' ); ?>
-				<!--/.CONTENT END-->
+<?php if ( have_posts() ): the_post() ?>
+	<section class="grey-bg" id="education">
+		<div class="container">
+			<div class="row">
+				<div class="col-md-3">
+					<h3 class="title-small">
+						<span><?php the_title(); ?></span>
+					</h3>
+					<p class="content-detail"></p>
+				</div>
+				<div class="col-md-9 content-right">
+					<div class="row">
+						<?php the_content(); ?>
+					</div>
+				</div>
 			</div>
-			<!--/.CONTENT-WRAP END-->
 		</div>
+	</section>
+<?php endif; ?>
 <?php get_footer(); ?>
 
 
