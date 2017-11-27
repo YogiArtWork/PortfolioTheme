e59a42ceb81a9334937a201323e3d8757ee4eba2
Ravinder Kumar <ravinder.kumar.apj@gmail.com>
2017-11-27 13:04:15 +0530

    Add preloader image

--- template-parts/preloader.php
+++ template-parts/preloader.php
@@ -1,7 +1,7 @@
 <!--PRELOADER-->
 <div id="preloader">
 	<div id="status">
-		<img alt="logo" src="<?php echo get_template_directory_uri(); ?>/assets/images/logo-big.png">
+		<img alt="logo" src="<?php echo get_template_directory_uri(); ?>/assets/images/yogi/yogi_logo_4.png">
 	</div>
 </div>
 <!--/.PRELOADER END-->
