2b1ee650d15da5445bc7212d361544a23c64aa30
Ravinder Kumar <ravinder.kumar.apj@gmail.com>
2017-11-27 13:03:54 +0530

    Add logo

--- template-parts/header.php
+++ template-parts/header.php
@@ -3,7 +3,7 @@
 	<div class="for-sticky">
 		<!--LOGO-->
 		<div class="col-md-2 col-xs-6 logo">
-			<a href="<?php echo esc_url( home_url('/') ); ?>"><img alt="logo" class="logo-nav" src="<?php echo get_template_directory_uri(); ?>/assets/images/logo.png"></a>
+			<a href="<?php echo esc_url( home_url('/') ); ?>"><img alt="logo" class="logo-nav" src="<?php echo get_template_directory_uri(); ?>/assets/images/yogi/yogi_logo_1.png"></a>
 		</div>
 		<!--/.LOGO END-->
 	</div>
